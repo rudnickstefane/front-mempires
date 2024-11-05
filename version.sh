041455eda27a33246edaaa712d0f07c5917cb63c
Rudnick Stefane <rudnick.nogueira@gmail.com>
2024-11-04 23:27:19 -0300

    [Master] Fix: Ajuste no versionInfo.

--- version.sh
+++ version.sh
@@ -142,9 +142,8 @@ git push origin "$new_revision" || { echo "[ERRO]: Falha ao fazer push da tag pa
 previous_revision=$(git tag | grep -v "$new_revision" | sort -V | tail -n 1)
 
 # Atualiza a Master com as alterações realizadas.
-git add .
-git commit -m "Master atualizada com a versão $new_revision"
-git push origin master
+git add CHANGELOG.md versionInfo.inf
+git commit --amend --no-edit
 
 echo
 echo "---------------- TAG DE VERSÃO ----------------"
